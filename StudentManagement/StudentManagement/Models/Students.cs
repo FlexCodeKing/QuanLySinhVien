48c615eaabf97be1b40fd83893e161ba76908376
FlexCodeKing <huyhoangphamnguyen1@gmail.com>
2024-04-05 23:12:41 +0700

    add Students models

--- StudentManagement/StudentManagement/Models/Students.cs
+++ StudentManagement/StudentManagement/Models/Students.cs
@@ -1,6 +1,19 @@
-﻿namespace StudentManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace StudentManagement.Models
 {
     public class Students
     {
+        [Key]
+        public int StudentsID {get; set;}
+
+        [Required]
+        public string StudentsName {  get; set; }
+        public string StudentsPhone { get; set; }
+        public string StudentsEmail { get; set; }
+        public string StudentsAddress { get; set; }
+
     }
 }
