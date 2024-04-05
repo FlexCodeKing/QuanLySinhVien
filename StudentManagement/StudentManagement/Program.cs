6f4b9e36d06ecc55e9d50066c8b1fb970b6c56b2
FlexCodeKing <huyhoangphamnguyen1@gmail.com>
2024-04-06 01:06:47 +0700

    change program cs

--- StudentManagement/StudentManagement/Program.cs
+++ StudentManagement/StudentManagement/Program.cs
@@ -1,7 +1,20 @@
+using StudentManagement.DataContexts;
+using StudentManagement.Services;
+using Microsoft.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddScoped<StudentServices>();
+
+builder.Services.AddScoped<StudentContexts>(provider =>
+{
+    string filePath = "CSV_File/studentinfo.csv";
+    return new StudentContexts(filePath);
+}
+);
+
 
 var app = builder.Build();
 
