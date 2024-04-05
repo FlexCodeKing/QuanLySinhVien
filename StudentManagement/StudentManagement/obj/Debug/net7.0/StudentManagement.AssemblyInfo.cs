c47ec5bcc752a8913bc203b7e49eb057668c558e
FlexCodeKing <huyhoangphamnguyen1@gmail.com>
2024-04-05 22:27:40 +0700

    add model folder

--- StudentManagement/StudentManagement/obj/Debug/net7.0/StudentManagement.AssemblyInfo.cs
+++ StudentManagement/StudentManagement/obj/Debug/net7.0/StudentManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c374fd0a6838c0b32c9f60d26723eeca9efc591")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
