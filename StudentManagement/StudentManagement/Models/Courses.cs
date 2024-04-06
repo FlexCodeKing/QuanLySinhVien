9217f05442dbb23fee44e5c6c27ef0ae9e44ebdb
MHieuHub7 <tranminhhieu071204@gmail.com>
2024-04-06 20:28:35 +0700

    add file Courses.cs

---
+++ StudentManagement/StudentManagement/Models/Courses.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentManagement.Models
+{
+    public class Courses
+    {
+        public string courseID { get; set; }
+        public string courseName { get; set; }
+        public string courseType { get; set; }
+        public string courseDescription { get; set; }
+        public string courseDuration { get; set; }
+        public string courseFee { get; set; }
+    }
+}
