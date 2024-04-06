8c3a6817f7c7be63b3b466afb7feffc709adfaab
FlexCodeKing <huyhoangphamnguyen1@gmail.com>
2024-04-06 23:19:06 +0700

    add view students

---
+++ StudentManagement/StudentManagement/Pages/Student/EditStudent.cshtml.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StudentManagement.Models;
+using StudentManagement.Services;
+
+namespace StudentManagement.Pages.Student
+{
+    public class EditStudentModel : PageModel
+    {
+        private readonly StudentServices _studentService;
+
+        public EditStudentModel(StudentServices studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [BindProperty]
+        public Students Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? studentId)
+        {
+            if(studentId == null)
+            {
+                return NotFound();
+            }
+            var student = _studentService.Student.FirstOrDefault(s => s.StudentsID == studentId);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            Student = student;
+            return Page();
+        }
+
+        public async Task<IActionResult>OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _studentService.UpdateStuden(Student.StudentsID, Student);
+            return RedirectToPage(nameof(Index));
+        }
+    }
+}
---
+++ StudentManagement/StudentManagement/Pages/Student/Student_information.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentManagement.Pages.Student
+{
+    public class Student_informationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
