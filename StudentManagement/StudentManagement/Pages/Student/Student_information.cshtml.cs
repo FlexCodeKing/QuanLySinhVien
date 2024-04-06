88b9c227ffef80a45ea558bef4a6b53d82699f2e
FlexCodeKing <huyhoangphamnguyen1@gmail.com>
2024-04-07 00:01:32 +0700

    add student information cshtml

--- StudentManagement/StudentManagement/Pages/Student/Student_information.cshtml.cs
+++ StudentManagement/StudentManagement/Pages/Student/Student_information.cshtml.cs
@@ -1,12 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StudentManagement.Models;
+using StudentManagement.Services;
 
 namespace StudentManagement.Pages.Student
 {
     public class Student_informationModel : PageModel
     {
+        private readonly StudentServices _studentService;
+
+        public IList<Students> StudentList { get; set; } = default;
+
+        [BindProperty]
+        public Students NewStudent { get; set; } = default!;
+
+        public Student_informationModel(StudentServices studentService)
+        {
+            _studentService = studentService;
+        }
         public void OnGet()
         {
+            StudentList = _studentService.GetStudent();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewStudent == null)
+            {
+                return Page();
+            }
+
+            _studentService.AddStudent(NewStudent);
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            _studentService.DeleteStudent(id);
+            return RedirectToAction("Get");
         }
     }
 }
