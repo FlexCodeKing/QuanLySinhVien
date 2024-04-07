f38a1b668b5f027f111b9609a1be8ddf02741d7c
FlexCodeKing <huyhoangphamnguyen1@gmail.com>
2024-04-07 17:39:55 +0700

    update Course models

--- StudentManagement/StudentManagement/Models/Courses.cs
+++ StudentManagement/StudentManagement/Models/Courses.cs
@@ -1,8 +1,12 @@
-﻿namespace StudentManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagement.Models
 {
     public class Courses
     {
+        [Key]
         public int ID { get; set; }
-        public string CourseName { get; set; }
+        [Required]
+        public string? CourseName { get; set; }
     }
 }
