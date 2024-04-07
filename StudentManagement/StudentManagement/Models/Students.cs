5b186998814b89f8b54bdb76c0bba0bab276ba40
FlexCodeKing <huyhoangphamnguyen1@gmail.com>
2024-04-07 17:26:11 +0700

    add

--- StudentManagement/StudentManagement/Models/Students.cs
+++ StudentManagement/StudentManagement/Models/Students.cs
@@ -11,10 +11,7 @@ namespace StudentManagement.Models
 
         [Required]
         public string? StudentsName {  get; set; }
-
-        [RegularExpression(@"^((\+84|84|0|0084){1})(3|5|7|8|9))+([0-9]{8})$", ErrorMessage = "Số điện thoại không hợp lệ.")]
         public string? StudentsPhone { get; set; }
-
         public string? StudentsEmail { get; set; }
         public string? StudentsAddress { get; set; }
 
