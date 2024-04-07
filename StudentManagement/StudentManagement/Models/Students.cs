2bed284801bdcb46a8c24535940d81148439cb5b
FlexCodeKing <huyhoangphamnguyen1@gmail.com>
2024-04-07 21:07:22 +0700

    fix student model 2

--- StudentManagement/StudentManagement/Models/Students.cs
+++ StudentManagement/StudentManagement/Models/Students.cs
@@ -12,7 +12,7 @@ namespace StudentManagement.Models
         [Required]
         public string? StudentsName {  get; set; }
 
-        [RegularExpression(@"^((\+84|84|0|0084){1})(3|5|7|8|9))+([0-9]{8})$", ErrorMessage = "Invalid Phone Number")]
+        [RegularExpression(@"^((\+84|84|0|0084){1})(3|5|7|8|9)+([0-9]{8})$", ErrorMessage = "Invalid Phone Number")]
         public string? StudentsPhone { get; set; }
 
         [RegularExpression(@"^[\w\.-]+@gmail\.com$", ErrorMessage = "Email must have @gmail.com")]
