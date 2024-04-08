e1a2de671813aed6b07b9837c96f4c43befd0852
FlexCodeKing <huyhoangphamnguyen1@gmail.com>
2024-04-09 04:24:12 +0700

    add Test

---
+++ StudentManagement/StudentManagement/Tests/ManagerTests.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Linq;
+using StudentManagement.DataContexts;
+using StudentManagement.Models;
+using StudentManagement.Services;
+using Xunit;
+
+namespace ManagerTests
+{
+    public class ManagerServiceTests
+    {
+        private readonly string testCsvFilePath = "test_managers.csv";
+
+        [Fact]
+        public void AddManager_ShouldAddManager()
+        {
+            // Arrange
+            var managerContext = new ManagerContext(testCsvFilePath);
+            var managerService = new ManagerService(managerContext);
+            var managerToAdd = new Manager
+            {
+                Name = "Test Manager",
+                Email = "testmanager@gmail.com",
+                Address = "Test Address",
+                Username = "testmanager",
+                Password = "testpassword"
+            };
+
+            // Act
+            managerService.AddManager(managerToAdd);
+
+            // Assert
+            var addedManager = managerContext.Managers.Last();
+            Assert.Equal("Test Manager", addedManager.Name);
+            Assert.Equal("testmanager@gmail.com", addedManager.Email);
+            Assert.Equal("Test Address", addedManager.Address);
+            Assert.Equal("testmanager", addedManager.Username);
+            Assert.Equal("testpassword", addedManager.Password);
+
+            // Clean up
+            /*File.Delete(testCsvFilePath);*/
+        }
+
+        [Fact]
+        public void UpdateManager_ShouldUpdateManager()
+        {
+            // Arrange
+            var managerContext = new ManagerContext(testCsvFilePath);
+            var managerService = new ManagerService(managerContext);
+
+            // Ensure there is a manager to update
+            var initialManager = new Manager
+            {
+                Name = "Initial Manager",
+                Email = "initialmanager@gmail.com",
+                Address = "Initial Address",
+                Username = "initialmanager",
+                Password = "initialpassword"
+            };
+            managerService.AddManager(initialManager);
+
+            var managerToUpdate = managerContext.Managers.First();
+            var updatedManager = new Manager
+            {
+                Name = "Updated Manager",
+                Email = "updatedmanager@gmail.com",
+                Address = "Updated Address",
+                Username = "updatedmanager",
+                Password = "updatedpassword"
+            };
+
+            // Act
+            managerService.UpdateManager(managerToUpdate.Id, updatedManager);
+
+            // Assert
+            var updatedManagerFromContext = managerContext.Managers.FirstOrDefault(m => m.Id == managerToUpdate.Id);
+            Assert.NotNull(updatedManagerFromContext);
+            Assert.Equal("Updated Manager", updatedManagerFromContext.Name);
+            Assert.Equal("updatedmanager@gmail.com", updatedManagerFromContext.Email);
+            Assert.Equal("Updated Address", updatedManagerFromContext.Address);
+            Assert.Equal("updatedmanager", updatedManagerFromContext.Username);
+            Assert.Equal("updatedpassword", updatedManagerFromContext.Password);
+
+            // Clean up
+            File.Delete(testCsvFilePath);
+        }
+
+        [Fact]
+        public void DeleteManager_ShouldDeleteManager()
+        {
+            // Arrange
+            var managerContext = new ManagerContext(testCsvFilePath);
+            var managerService = new ManagerService(managerContext);
+
+            // Ensure there is a manager to delete
+            var managerToDelete = new Manager
+            {
+                Name = "Manager to Delete",
+                Email = "deletemanager@gmail.com",
+                Address = "Delete Address",
+                Username = "deletemanager",
+                Password = "deletepassword"
+            };
+            managerService.AddManager(managerToDelete);
+
+            // Act
+            managerService.DeleteManager(managerToDelete.Id);
+
+            // Assert
+            var deletedManagerFromContext = managerContext.Managers.FirstOrDefault(m => m.Id == managerToDelete.Id);
+            Assert.Null(deletedManagerFromContext);
+
+            // Clean up
+            /*File.Delete(testCsvFilePath);*/
+        }
+    }
+}
---
+++ StudentManagement/StudentManagement/Tests/StudentsTests.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Linq;
+using StudentManagement.DataContexts;
+using StudentManagement.Models;
+using StudentManagement.Services;
+using Xunit;
+
+namespace StudentTests
+{
+    public class StudentServiceTests
+    {
+
+        private readonly string testCsvFilePath = "test_students.csv";
+
+        [Fact]
+        public void AddStudent_ShouldAddStudent()
+        {
+            // Arrange
+            var studentContext = new StudentContexts(testCsvFilePath);
+            var studentServices = new StudentServices(studentContext);
+            var studentToAdd = new Students
+            {
+                StudentsName = "Test Student",
+                StudentsPhone = "1234567890",
+                StudentsEmail = "test@gmail.com",
+                StudentsAddress = "Test Address",
+                Studentsusername = "testuser",
+                Studentspassword = "testpassword"
+            };
+
+            // Act
+            studentServices.AddStudent(studentToAdd);
+
+            // Assert
+            var addedStudent = studentContext.Student.Last();
+            Assert.Equal("Test Student", addedStudent.StudentsName);
+            Assert.Equal("1234567890", addedStudent.StudentsPhone);
+            Assert.Equal("test@gmail.com", addedStudent.StudentsEmail);
+            Assert.Equal("Test Address", addedStudent.StudentsAddress);
+            Assert.Equal("testuser", addedStudent.Studentsusername);
+            Assert.Equal("testpassword", addedStudent.Studentspassword);
+
+            // Clean up
+            /*File.Delete(testCsvFilePath);*/
+        }
+
+        [Fact]
+        public void UpdateStudent_ShouldUpdateStudent()
+        {
+            // Arrange
+            var studentContext = new StudentContexts(testCsvFilePath);
+            var studentServices = new StudentServices(studentContext);
+
+            // Ensure there is a student to update
+            var initialStudent = new Students
+            {
+                StudentsName = "Initial Name",
+                StudentsPhone = "1234567890",
+                StudentsEmail = "initial@gmail.com",
+                StudentsAddress = "Initial Address",
+                Studentsusername = "initialuser",
+                Studentspassword = "initialpassword"
+            };
+            studentServices.AddStudent(initialStudent);
+
+            var studentToUpdate = studentContext.Student.First();
+            var updatedStudent = new Students
+            {
+                StudentsName = "Updated Name",
+                StudentsPhone = "9876543210",
+                StudentsEmail = "updated@gmail.com",
+                StudentsAddress = "Updated Address",
+                Studentsusername = "updateduser",
+                Studentspassword = "updatedpassword"
+            };
+
+            // Act
+            studentServices.UpdateStudent(studentToUpdate.StudentsID, updatedStudent);
+
+            // Assert
+            var updatedStudentFromContext = studentContext.Student.FirstOrDefault(s => s.StudentsID == studentToUpdate.StudentsID);
+            Assert.NotNull(updatedStudentFromContext);
+            Assert.Equal("Updated Name", updatedStudentFromContext.StudentsName);
+            Assert.Equal("9876543210", updatedStudentFromContext.StudentsPhone);
+            Assert.Equal("updated@gmail.com", updatedStudentFromContext.StudentsEmail);
+            Assert.Equal("Updated Address", updatedStudentFromContext.StudentsAddress);
+            Assert.Equal("updateduser", updatedStudentFromContext.Studentsusername);
+            Assert.Equal("updatedpassword", updatedStudentFromContext.Studentspassword);
+
+            // Clean up
+            File.Delete(testCsvFilePath);
+        }
+
+
+        [Fact]
+        public void DeleteStudent_ShouldDeleteStudent()
+        {
+            // Arrange
+            var studentContext = new StudentContexts(testCsvFilePath);
+            var studentServices = new StudentServices(studentContext);
+            var studentToDelete = studentContext.Student.First();
+
+            // Act
+            studentServices.DeleteStudent(studentToDelete.StudentsID);
+
+            // Assert
+            var deletedStudentFromContext = studentContext.Student.FirstOrDefault(s => s.StudentsID == studentToDelete.StudentsID);
+            Assert.Null(deletedStudentFromContext);
+
+            // Clean up
+            /*File.Delete(testCsvFilePath);*/
+        }
+    }
+}
